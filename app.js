8e4e732794d4ae56003dfde2e9ae39abca8a5232
Andrew Provis <aprovis@dresources.com>
2015-07-17 12:00:04 +0100

    Created a controller for the app and a gem product with product information

--- app.js
+++ app.js
@@ -2,4 +2,20 @@
  * Created by aprovis on 17/07/15.
  */
 
-var app = angular.module('store', []);
+(function () {
+    // Creating our Store module which will run the Gem Store/Shop
+    var app = angular.module('store', []);
+
+    // Creating our controller to handle sending gem information to our storefront.
+    app.controller('StoreController', function () {
+        this.product = gem;
+    });
+
+    // Created a variable to hold product information of a gem.
+    var gem = {
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: 'It\'s a gem... a really shiny gem. What more could you want?'
+    }
+
+})();
